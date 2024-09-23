d3bde82e2be8ca3950a7fa0f65dbcd2e246f35d6
João Gabriel Pereira <pereirajoaogabriel2007@gmail.com>
2024-09-23 15:31:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+1-
+let numero;
+let mult=0;
+numero= Number(prompt("insira o numero para ter a tabuada"));
+
+while(mult<=10){
+    let result=numero*mult;
+    mult++
+    alert(result)
+}
+
+2-
+let n=1, result
+let j=1
+
+while (n<= 9){
+    console.log('tabuada do ' +n)
+    j=1
+    while(j<=10){
+        
+        result=n*j
+        
+        console.log(n + 'x'+ j +'=' + result)
+    j++
+    }
+
+    n++
+}
+*/
